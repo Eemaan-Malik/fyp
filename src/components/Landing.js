d7e7d143bfae0be7521fbdd5451d62d69feb1a6a
Eemaan-Malik <emaanmalik1507@gmail.com>
2025-03-29 19:20:31 +0500

    Add files via upload

---
+++ src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../index.css'; 
+
+function Landing() {
+  return (
+    <header className="landing">
+      <h1 className="title">
+        <span className="rounded-title">Architext:</span>
+      </h1>
+      <p className="subtitle">
+        <span className="rounded-subtitle">Floorplan Generator Using Linguistics Only</span>
+      </p>
+    </header>
+  );
+}
+
+export default Landing;
